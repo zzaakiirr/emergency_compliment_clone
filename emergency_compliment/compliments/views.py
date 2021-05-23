cd040c977208cbf96d05990ff469127df6d1dbd6
Zakir Dzhamaliddinov <pfrbhxbr@gmail.com>
2021-05-23 11:59:08 +0300

    Fixed color updating, but very poor(

--- emergency_compliment/compliments/views.py
+++ emergency_compliment/compliments/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -8,6 +10,7 @@ from .models import Compliment
 
 def index(request):
     context = {'compliment': __get_new_compliment()}
+    __set_bg_color(request.session)
     return render(request, 'index.html', context)
 
 
@@ -23,7 +26,14 @@ def get_new_compliment(request):
         response = {'result': False, 'error': 'Cannot get new compliment'}
         return JsonResponse(response, status=500)
 
-    response = {'result': True, 'new_compliment': new_compliment.text}
+    __set_bg_color(request.session)
+    response = {
+        'result': True,
+        'new_compliment': new_compliment.text,
+        # FIX: For some reason JS reads 'bg_color' from session as old value
+        #      So we send it back for further config in JS
+        'bg_color': request.session.get('bg_color'),
+    }
     return JsonResponse(response, status=200)
 
 
@@ -38,3 +48,16 @@ def __get_new_compliment(old_compliment_text=None):
 
         if new_compliment.text != old_compliment_text or Compliment.objects.count() == 1:
             return new_compliment
+
+
+def __set_bg_color(session):
+    BG_COLORS = ['#F2ED53', '#E5B156', '#B4CF4E']
+    current_bg_color = session.get('bg_color')
+
+    if not current_bg_color:
+        session['bg_color'] = BG_COLORS[0]
+        return
+
+    session['bg_color'] = random.choice(
+        [color for color in BG_COLORS if color != current_bg_color]
+    )
