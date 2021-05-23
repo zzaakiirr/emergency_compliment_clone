fe15cf4f2a4361df387c8bda23a795ada0b5748d
Zakir Dzhamaliddinov <pfrbhxbr@gmail.com>
2021-05-23 13:06:30 +0300

    Minor fix

--- emergency_compliment/compliments/urls.py
+++ emergency_compliment/compliments/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ajax/new_compliment', views.get_new_compliment, name="get_new_compliment")
+    path('ajax/new_compliment', views.get_new_compliment, name="get_new_compliment"),
 ]
